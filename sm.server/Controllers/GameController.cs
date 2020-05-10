87d6c831ea7c92dbf3b1967003cd56823d04f26e
novakvova <novakvova@gmail.com>
2020-05-10 11:11:27 +0300

    Base games

---
+++ sm.server/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sm.server.Models;
+
+namespace sm.server.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class GameController : ControllerBase
+    {
+        private static List<Solider> list = new List<Solider>();
+
+        [HttpGet("{nick}")]
+        public IActionResult Index(string nick)
+        {
+            var solider = list.FirstOrDefault(s => s.Nick == nick);
+            if (solider != null)
+                list.Remove(solider);
+            return Ok(solider);
+        }
+
+        //Стриляємо по обєкту
+        [HttpPost]
+        public IActionResult Post([FromBody]Solider solider)
+        {
+            if (solider != null)
+                list.Add(solider);
+            return Ok();
+        }
+    }
+}
